ab12caa816b4cc01b2f9929c0f2e98c55ed6fd17
Zhen Tian <zhen.tian@vialto.com>
2024-02-04 22:12:16 +0800

    init

---
+++ TryHttpClientWithReqCookie/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// var noCookieHandler = new HttpClientHandler();
+// noCookieHandler.UseCookies = false;
+// var client = new HttpClient(noCookieHandler);
+var client = new HttpClient();
+var setCookieUrl = "https://httpbin.org/cookies/set/aff/node1";
+var listCookiesUrl = "https://httpbin.org/cookies";
+
+var resp = await client.GetAsync(setCookieUrl);
+Console.WriteLine($"first resp: {await resp.Content.ReadAsStringAsync()}");
+
+var secondResp = await client.GetAsync(listCookiesUrl);
+Console.WriteLine($"second resp: {await secondResp.Content.ReadAsStringAsync()}");
+
+var newClientResp = await new HttpClient().GetAsync(listCookiesUrl);
+Console.WriteLine($"new client resp: {await newClientResp.Content.ReadAsStringAsync()}");
